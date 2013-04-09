151972c8fa66762f21f2074ec9294b8383c00e15
Monica Sharma <monicasharma1053@gmail.com>
2013-04-09 15:21:14 -0500

    files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WsBookstore::Application.config.secret_token = '79c9ee7845f62613a0af6d87ad58892363ac7f5d8534b12e77c10d0fd6fbe9d7d6fafc6c60852ab643f31865cd34512aee8226aa6db6c2f49550a61505840c23'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WsBookstore::Application.config.session_store :cookie_store, key: '_ws_bookstore_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WsBookstore::Application.config.session_store :active_record_store
