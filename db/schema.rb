7471ac70f90064fbfe451ef8b4c1840e982dfb91
Monica Sharma <monicasharma1053@gmail.com>
2013-04-09 15:38:44 -0500

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130409203524) do
+ActiveRecord::Schema.define(:version => 20130409203653) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(:version => 20130409203524) do
     t.string   "isbn"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "orders", :force => true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "payment"
+    t.string   "status"
+    t.decimal  "total"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
