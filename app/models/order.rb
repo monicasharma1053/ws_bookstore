cb0fa656063f56ed317f437d4f42ffd03ef533ef
Monica Sharma <monicasharma1053@gmail.com>
2013-04-15 12:59:41 -0500

    heroku link

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ActiveRecord::Base
   attr_accessible :address, :name, :payment, :status, :total
 
-  has_many :carts, dependent: :destroy 
+  has_many :carts, dependent: :destroy
   has_many :books, through: :carts
 
   before_create :set_total_and_status
